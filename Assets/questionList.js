5b1a8247b8238b9c9e3aad980c11d347d6fe5042
Tyler Bantley <tyler.bantley@icloud.com>
2021-12-21 14:34:25 -0600

    second commit w/ questions

---
+++ Assets/questionList.js
@@ -0,0 +1,41 @@
+let list = [{
+    question: "What word best refers to the rules that define the structure of a programming language?",
+    choice1: "Synchronize", 
+    choice2: "Syntax", 
+    choice3: "Structure",
+    choice4: "Solve", 
+    answer: 1 
+},
+    {
+            question: "Where in an HTML document is the correct place to refer to an external style sheet?", 
+    choice1: "footer",
+    choice2: "body",
+    choice3: "head",
+    choice4: "section",
+    answer: 2
+}, 
+    {
+            question: "How does a FOR loop start?",
+    choice1: "for (i <= 5;i++)", 
+    choice2: "for i = 1 to 5", 
+    choice3: "for (i = 0; i <= 5; i++)", 
+    choice4: "for (i = 0; i <= 5)", 
+    answer: 2
+}, 
+    {
+            question: "What is the correct HTML element for inserting a line break?", 
+    choice1: "<lb>", 
+    choice2: "<break>", 
+    choice3: "<bre>", 
+    choice4: "<br>", 
+    answer: 3
+}, 
+    {
+            question: "How do you make each word in a text start with a capital letter?" , 
+    choice1: "text-style:capitalize",
+    choice2: "transform:capitalize", 
+    choice3: "text-transform:capitalize", 
+    choice4: "you cant", 
+    answer: 2
+}];
+
---
+++ Assets/script.js
@@ -0,0 +1,116 @@
+
+//start 
+let start = document.querySelector("#start");
+
+//rules 
+let rules = document.querySelector("#rules");
+let exit = document.querySelector("#exit");
+let continueBtn = document.querySelector("#continue");
+
+//trivia 
+let trivia = document.querySelector("#trivia");
+let time = document.querySelector("#time");
+
+//questions 
+let questNum = document.querySelector("#questNum");
+let questText = document.querySelector("#questText");
+
+//choices
+let option1 = document.querySelector("#option1");
+let option2 = document.querySelector("#option2");
+let option3 = document.querySelector("#option3");
+let option4 = document.querySelector("#option4");
+
+//correct and next
+let correctTotal = document.querySelector("#correctTotal");
+let next = document.querySelector("#next");
+
+//result
+let results = document.querySelector("#results");
+let points = document.querySelector("#points");
+let quit = document.querySelector("#quit");
+let again = document.querySelector("#again");
+
+let options = document.querySelectorAll(".options");
+
+let index = 0;
+let clock = 0;
+let interval = 0;
+
+let correct = 0;
+
+
+let userAns = undefined;
+
+start.addEventListener("click" , ()=>{
+    start.style.display = "none";
+    rules.style.display = "block";
+});
+
+exit.addEventListener("click" , ()=>{
+    start.style.display = "block";
+    rules.style.display = "none";
+});
+
+let countDown = ()=>{
+    if(clock === 15)
+    {
+        clearInterval(interval);
+        next.click();
+    }
+    else 
+        {
+            clock++;
+            time.innerText = clock;
+        }
+}
+
+
+let loadData = ()=>{
+    questNum.innerText = index + 1 + ". ";
+    questText.innerText = list[index].question;
+    option1.innerText = list[index].choice1;
+    option2.innerText = list[index].choice2;
+    option3.innerText = list[index].choice3;
+    option4.innerText = list[index].choice4;
+
+    
+    clock = 0; 
+}
+
+loadData();
+
+
+
+
+continueBtn.addEventListener("click", () => {
+    trivia.style.display = "block";
+    rules.style.display = "none";
+
+    interval = setInterval(countDown, 1000);
+    loadData();
+});
+
+options.forEach(removeActive => {
+    removeActive.classList.remove("active");
+    })
+
+    correctTotal.innerHTML = `${correct = 0} Out of ${list.length} Questions`;
+
+
+options.forEach( (choices,choiceNum) =>{
+    choices.addEventListener("click" , ()=>{
+        choices.classList.add("active");
+        
+        if(choiceNum === list[index].answer)
+        {
+            correct++;
+        }
+        else
+            {
+                correct += 0;
+            }
+            
+            clearInterval(interval)
+    })
+}); 
